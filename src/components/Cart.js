8f4892c3fe3d2029cca8cf605541ffd0443fb44b
ishween <ishweensehmbhi@gmail.com>
2022-04-04 11:10:10 -0400

    remove unnecessary files

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,37 +1,37 @@
-// import firebase from "../firebase";
-// import { getDatabase, onValue, ref } from "firebase/database";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faClose } from "@fortawesome/free-solid-svg-icons";
-// import { useState, useEffect } from "react";
+import firebase from "../firebase";
+import { getDatabase, onValue, ref } from "firebase/database";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
 
-// function Cart() {
-// 	const [cartItems, setCartItems] = useState({});
+function Cart() {
+	const [cartItems, setCartItems] = useState({});
 
-// 	useEffect(() => {
-// 		// get a snapshot of the database
-// 		const database = getDatabase(firebase);
-// 		// make an inquiry at this node
-// 		const dbRef = ref(database, `/user/basket/current/`);
+	useEffect(() => {
+		// get a snapshot of the database
+		const database = getDatabase(firebase);
+		// make an inquiry at this node
+		const dbRef = ref(database, `/user/basket/current/`);
 
-// 		onValue(dbRef, (res) => {
-// 			// this returns the items as an object of objects
-// 			const newCart = res.val();
+		onValue(dbRef, (res) => {
+			// this returns the items as an object of objects
+			const newCart = res.val();
 
-// 			// setCurrentInventory(newState);
-// 			setCartItems(newCart);
-// 			console.log(newCart);
-// 		});
-// 	}, []);
+			setCurrentInventory(newState);
+			setCartItems(newCart);
+			console.log(newCart);
+		});
+	}, []);
 
-// 	return (
-// 		<section className="shopping-cart">
-// 			<button>
-// 				<FontAwesomeIcon icon={faClose} />
-// 			</button>
-// 			<h2>Items In Your Cart</h2>
-// 			<hr />
-// 		</section>
-// 	);
-// }
+	return (
+		<section className="shopping-cart">
+			<button>
+				<FontAwesomeIcon icon={faClose} />
+			</button>
+			<h2>Items In Your Cart</h2>
+			<hr />
+		</section>
+	);
+}
 
-// export default Cart;
+export default Cart;
